cf5a6ad6d96135130d2faed93e11f48aa7c18f12
halyna <galinabandurka7@gmail.com>
2025-07-14 09:36:14 +0200

    Refactor authenticate middleware to support cookies and headers

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -3,29 +3,45 @@ import { SessionsCollection } from '../models/session.js';
 import { UsersCollection } from '../models/user.js';
 
 export const authenticate = async (req, res, next) => {
+  let token;
+  let session;
+
   const bearerToken = req.get('authorization');
 
-  if (!bearerToken)
-    return next(createHttpError(401, 'Please provide Authorization header'));
+  if (bearerToken) {
+    const [bearer, extractedToken] = bearerToken.split(' '); 
+    if (bearer !== 'Bearer' || !extractedToken) {
+      return next(createHttpError(401, 'Authorization header should be of type Bearer'));
+    }
+    token = extractedToken;
+    session = await SessionsCollection.findOne({ accessToken: token });
+  } else {
+    const sessionId = req.cookies?.sessionId;
+    if (!sessionId) {
+      return next(createHttpError(401, 'No Authorization header or sessionId cookie found'));
+    }
+    session = await SessionsCollection.findById(sessionId);
+  }
 
-  const [bearer, token] = bearerToken.split(' ');
-
-  if (bearer !== 'Bearer' || !token)
-    return next(createHttpError(401, 'Authorization header should be of type Bearer'));
-
-  const session = await SessionsCollection.findOne({ accessToken: token });
-
-  if (!session) return next(createHttpError(401, 'Session not found'));
+  if (!session) {
+    return next(createHttpError(401, 'Session not found'));
+    
+  }
 
   const isTokenExpired = new Date() > new Date(session.accessTokenValidUntil);
 
-  if (isTokenExpired) return next(createHttpError(401, 'Access token expired'));
+  if (isTokenExpired) {
+    return next(createHttpError(401, 'Access token expired'));
+    
+  }
 
   const user = await UsersCollection.findById(session.userId);
 
-  if (!user) return next(createHttpError(401, 'User not found'));
+  if (!user) {
+    return next(createHttpError(401, 'User not found')); 
+    
+  }
 
   req.user = user;
-
   next();
 };
